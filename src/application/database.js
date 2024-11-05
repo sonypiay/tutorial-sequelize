a11532f1550fe7b1b96b9c8ee22ff85ccc6d53f6
sonypiay <pyscho30@gmail.com>
2024-11-06 02:32:25 +0700

    adjust connection database

--- src/application/database.js
+++ src/application/database.js
@@ -3,14 +3,17 @@ import 'dotenv/config';
 
 const credential = {
     host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     driver: process.env.DB_DRIVER
 };
 
-const connectionString = `${credential.driver}://${credential.username}:${credential.password}@${credential.host}:${credential.port}/${credential.database}`;
-const connection = new Sequelize(connectionString);
 
-export default connection;
+const connectionString = `${credential.driver}://${credential.username}:${credential.password}@${credential.host}:${credential.port}/${credential.database}`;
+const Connection = new Sequelize(connectionString, {
+    logging: (...msg) => console.log(msg)
+});
+
+export default Connection;

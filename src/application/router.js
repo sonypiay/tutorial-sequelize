6fc19b426c806b66164aae91e11b2817a747985e
sonypiay <pyscho30@gmail.com>
2024-11-06 15:56:39 +0700

    add cors middleware

--- src/application/router.js
+++ src/application/router.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import { Middleware } from '../middleware/middleware.js';
 import { userRoute } from '../routes/user-route.js';
 
 export const router = express.Router();
 
+router.use(cors());
 router.use(userRoute);
 router.use(Middleware.errorException);

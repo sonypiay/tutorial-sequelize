5d6e356bfc5fd26b6e2f1b0dfc6c63e6a162c5af
sonypiay <pyscho30@gmail.com>
2024-11-06 15:57:03 +0700

    add validate api token middleware

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,4 +1,6 @@
+import { request } from "express";
 import ResponseError from "../exceptions/response-error.js";
+import UsersModel from "../models/users-model.js";
 
 export const Middleware = {};
 
@@ -16,4 +18,39 @@ Middleware.errorException = async(err, request, response, next) => {
         errors: responseData.message
     })
     .end();
+};
+
+Middleware.validateApiToken = async(request, response, next) => {
+    const getAuthorization = request.get('authorization');
+    
+    if( ! getAuthorization ) {
+        response.status(401).json({
+            errors: 'Unauthorized'
+        }).end();
+    } else {
+        const getBasicAuth = getAuthorization.replace('Basic ', '');
+        const decodeToken = Buffer.from(getBasicAuth, 'base64').toString('ascii');
+        const parsingToken = decodeToken.split(':');
+
+        const userId = parsingToken[0] ?? null;
+        const apiToken = parsingToken[1] ?? null;
+
+        const verifyToken = await UsersModel.count({
+            where: {
+                id: userId,
+                api_token: apiToken
+            }
+        });
+
+        if( verifyToken === 0 ) {
+            response.status(401).json({
+                errors: 'Unauthorized'
+            }).end();          
+        } else {
+            request.apiToken = apiToken;
+            request.userId = userId;
+            
+            next();
+        }
+    }
 };
